01a84f632fb767987176328ac2895ce7ec1e280a
Linet Oliech <linetaoliech@gmail.com>
2023-02-11 22:50:13 +0300

    Completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
+# setter method
+def name=(name)
+    @name = name
+  end
+
+  # getter method
+  def name
+    @name
+  end
+
+  def job=(value)
+    @job = value
+  end
+
+  def job
+    @job
+  end
 
 end
